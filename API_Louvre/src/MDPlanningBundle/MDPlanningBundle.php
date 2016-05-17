9016aca3ae46620769002a422d5c4f18daca5727
Marco <marc.declercq@laposte.net>
2016-05-17 10:10:45 +0200

    Nettoyage Bundle devenu inutile et ajout boutons facebook et twitter

--- API_Louvre/src/MDPlanningBundle/MDPlanningBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MDPlanningBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MDPlanningBundle extends Bundle
-{
-}
--- API_Louvre/web/bundles/MDPlanning/js/myEvents.js
+++
@@ -1,7 +0,0 @@
-console.log("Initialisation détéction des clic sur dates du calendrier");
-// Récupération des cellules 'Jour' du calendrier actif
-//var $calendrier = document.getElementById('calendar-holder');
-//var $tableHeader = $calendrier.getElementsByTagName('table');
-var $jTables=$('table');
-$jTables.css("color","yellow");
-console.log($jTables);
